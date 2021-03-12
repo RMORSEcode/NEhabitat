47386c86033eb6e69ef6d53e88cd483f7d697083
RMORSEcode <morsere01@gmail.com>
2021-03-12 14:06:42 -0500

    update fall haddock models

--- Fall_haddock_HGAMs.R
+++ Fall_haddock_HGAMs.R
@@ -37,6 +37,34 @@ barplot(rev(sort(table(test3[1:5,]))))
 # add BOTTEMP
 ## done
 
+impvarlist2=impvarlist[grepl("*Haddock_full_SPRING*", impvarlist)] # select all stages from a season
+xxi=3
+test2=read.csv(paste(wd,'/',impvarlist[xxi],sep=''), stringsAsFactors = F, row.names = 1)
+test3=matrix(data=NA, nrow=dim(test2)[1], ncol=dim(test2)[2])
+for (i in 1:dim(test2)[2]){
+  test3[,i]=rownames(test2)[rev(order(test2[,i]))]
+}
+colnames(test3)=colnames(test2)
+# table(test3[1:7,])
+im1=rev(sort(table(test3[1:7,])))
+im2=rev(sort(table(test3[1:7,])))
+im3=rev(sort(table(test3[1:7,])))
+
+test1=data.frame(im1) %>% filter(Freq>=3)
+test2=data.frame(im2) %>% filter(Freq>=3)
+test3=data.frame(im3) %>% filter(Freq>=3)
+
+# Reduce(intersect, list(test1[,1], test3[,1], test2[,1])) # too limiting...
+## this isnt working right, need to sort first I think
+# t4=test1 %>% full_join(test2, keep=T, by = c("Var1", "Freq"))
+# t5=t4 %>% full_join(test3, keep=T, by = c("Var1.x"="Var1", "Freq.x"="Freq"))
+
+x1=sort(as.character(test1$Var1))
+x2=sort(as.character(test2$Var1))
+x3=sort(as.character(test3$Var1))
+xf=paste(c(x1,x2,x3))
+unique(xf)
+
 
 ### UPDATE DATA to remove ichtyhyoplankton for fall haddock (only 6 positive samples 1977-2017)
 trainPA=trainPA %>% filter(Stg!='ich') %>% droplevels(exclude="ich")
